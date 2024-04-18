d64feb206275b7197ef0f12df05b1c1494e73cd6
Jaynscc <w0470637@nscc.ca>
2024-04-18 18:34:49 -0300

    update

---
+++ little-lemon/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TableBooking from './TableBooking';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TableBooking />
+    </div>
+  );
+}
+
+export default App;
